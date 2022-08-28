76c1bfa3c47b3556112108181ecbcb61cd12bc37
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2022-08-28 10:53:46 -0500

    Install emacs-nativecom instead of emacs-git

--- install_text_editors.fish
+++ install_text_editors.fish
@@ -1,5 +1,5 @@
 function install_text_editors -d 'Install the text editors I use'
-    set -l pkglist nano micro helix emacs-git
+    set -l pkglist nano micro helix emacs-nativecomp
     for pkg in pkglist
         __check_if_installed $pkg
 
